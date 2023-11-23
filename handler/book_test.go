1b75ed9795339ec41faecc7c2e0d1446bef678d0
Alvin Febriando <alvin.febriando@shopee.com>
2023-11-23 17:17:07 +0700

    test: test add product

--- handler/book_test.go
+++ handler/book_test.go
@@ -1,10 +1,15 @@
 package handler_test
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
+	"git.garena.com/sea-labs-id/bootcamp/batch-02/shared-projects/library-api/apperror"
+	"git.garena.com/sea-labs-id/bootcamp/batch-02/shared-projects/library-api/dto"
 	"git.garena.com/sea-labs-id/bootcamp/batch-02/shared-projects/library-api/entity"
 	"git.garena.com/sea-labs-id/bootcamp/batch-02/shared-projects/library-api/handler"
 	"git.garena.com/sea-labs-id/bootcamp/batch-02/shared-projects/library-api/mocks"
@@ -47,6 +52,67 @@ func (s *BookHandlerTestSuite) TestListBooks() {
 		s.router.ServeHTTP(s.rec, req)
 
 		s.Equal(http.StatusOK, s.rec.Code)
+	})
+}
+func (s *BookHandlerTestSuite) TestAddBook() {
+	s.Run("should return 201", func() {
+		quantity := 1
+		request := dto.BookRequest{
+			Title:       "A",
+			Description: "B",
+			Quantity:    &quantity,
+		}
+		body, _ := json.Marshal(request)
+		s.bu.On("AddBook", mock.AnythingOfType("*entity.Book")).Return(&entity.Book{}, nil)
+
+		req, _ := http.NewRequest(http.MethodPost, "/books", bytes.NewReader(body))
+		s.router.ServeHTTP(s.rec, req)
+
+		s.Equal(http.StatusCreated, s.rec.Code)
+	})
+	s.Run("should return 400", func() {
+		quantity := -1
+		request := dto.BookRequest{
+			Title:       "A",
+			Description: "B",
+			Quantity:    &quantity,
+		}
+		body, _ := json.Marshal(request)
+
+		req, _ := http.NewRequest(http.MethodPost, "/books", bytes.NewReader(body))
+		s.router.ServeHTTP(s.rec, req)
+
+		s.Equal(http.StatusBadRequest, s.rec.Code)
+	})
+	s.Run("should return 409", func() {
+		quantity := 1
+		request := dto.BookRequest{
+			Title:       "A",
+			Description: "B",
+			Quantity:    &quantity,
+		}
+		body, _ := json.Marshal(request)
+		s.bu.On("AddBook", mock.AnythingOfType("*entity.Book")).Return(nil, apperror.ErrAlreadyExist{})
+
+		req, _ := http.NewRequest(http.MethodPost, "/books", bytes.NewReader(body))
+		s.router.ServeHTTP(s.rec, req)
+
+		s.Equal(http.StatusConflict, s.rec.Code)
+	})
+	s.Run("should return 500", func() {
+		quantity := 1
+		request := dto.BookRequest{
+			Title:       "A",
+			Description: "B",
+			Quantity:    &quantity,
+		}
+		body, _ := json.Marshal(request)
+		s.bu.On("AddBook", mock.AnythingOfType("*entity.Book")).Return(nil, errors.New(""))
+
+		req, _ := http.NewRequest(http.MethodPost, "/books", bytes.NewReader(body))
+		s.router.ServeHTTP(s.rec, req)
+
+		s.Equal(http.StatusInternalServerError, s.rec.Code)
 	})
 }
 
