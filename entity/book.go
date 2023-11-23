d96c139ff77116290bbe91ef8786fcd394eba7cf
Alvin Febriando <alvin.febriando@shopee.com>
2023-11-23 14:26:56 +0700

    fix: fix gorm struct tags on entity

--- entity/book.go
+++ entity/book.go
@@ -3,9 +3,9 @@ package entity
 type Cover string
 
 type Book struct {
-	Id          uint   `json:"id" gorm:"id"`
-	Title       string `json:"title" gorm:"title"`
-	Description string `json:"description" gorm:"description"`
-	Quantity    int    `json:"quantity" gorm:"quantity"`
-	Cover       Cover  `json:"cover" gorm:"cover"`
+	Id          uint   `json:"id" gorm:"primaryKey;autoIncrement"`
+	Title       string `json:"title" gorm:"not null"`
+	Description string `json:"description" gorm:"not null"`
+	Quantity    int    `json:"quantity" gorm:"not null"`
+	Cover       Cover  `json:"cover" gorm:"not null"`
 }
