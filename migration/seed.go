1aac644458379ed5f9ceec00aa5fdfbd0f750ca2
Alvin Febriando <alvin.febriando@shopee.com>
2023-11-24 12:38:40 +0700

    feat: seed user data

--- migration/seed.go
+++ migration/seed.go
@@ -15,6 +15,12 @@ func Seed(db *gorm.DB) {
 		{Title: "How to eat", Description: "Explain how to eat", Quantity: 2, Cover: "kertas", AuthorId: 1},
 		{Title: "How to drink", Description: "Explain how to drink", Quantity: 3, Cover: "Kertas", AuthorId: 2},
 	}
+	users := []*entity.User{
+		{Name: "Alice", Email: "alice@example.com", Phone: "085346727162"},
+		{Name: "Bob", Email: "bob@example.com", Phone: "085212819384"},
+		{Name: "Charlie", Email: "charlie@example.com", Phone: "081394839283"},
+	}
 	db.Create(authors)
 	db.Create(books)
+	db.Create(users)
 }
