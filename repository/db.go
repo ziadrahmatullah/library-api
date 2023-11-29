65fb6f52b897dad6f7f2f2c26c08ecb5abf4b0ae
Alvin Febriando <alvin.febriando@shopee.com>
2023-11-29 23:23:06 +0700

    feat: only use db log when in dev environment

--- repository/db.go
+++ repository/db.go
@@ -1,6 +1,8 @@
 package repository
 
 import (
+	"os"
+
 	"git.garena.com/sea-labs-id/bootcamp/batch-02/shared-projects/library-api/util"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -9,7 +11,11 @@ import (
 
 func GetConnection() (*gorm.DB, error) {
 	dsn := util.GetDsn()
+	var l = logger.Default.LogMode(logger.Info)
+	if os.Getenv("ENV") == "dev" {
+		l = nil
+	}
 	return gorm.Open(postgres.Open(dsn), &gorm.Config{
-		Logger: logger.Default.LogMode(logger.Info),
+		Logger: l,
 	})
 }
