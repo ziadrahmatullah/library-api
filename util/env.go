46e1ae49e8f23cc93e1707b93237f4c9e44a4ee3
Alvin Febriando <alvin.febriando@shopee.com>
2023-12-14 12:52:57 +0700

    refactor: use env from docker

--- util/env.go
+++ util/env.go
@@ -1,16 +1,9 @@
 package util
 
 import (
-	"fmt"
 	"os"
-
-	"github.com/joho/godotenv"
 )
 
 func Getenv(key string) string {
-	err := godotenv.Load()
-	if err != nil {
-		fmt.Println(err)
-	}
 	return os.Getenv(key)
 }
